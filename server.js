b96c92c90f55299129c82da2f3fd26d54c45ae37
nader <nader90akhgari@gmail.com>
2021-04-29 14:17:37 +0100

    add pagination in the server

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const paginatedResults = (req, data) => {
 
   const results = {};
 
-  if (end < data.length) {
+  if (end > data.length) {
     results.next = {
       page: page + 1,
       limit: limit,
@@ -29,12 +29,18 @@ const paginatedResults = (req, data) => {
       limit: limit,
     };
   }
-  results.results = data.slice(start, end);
+  if (limit >= data.length) {
+    results.pages = 1;
+    results.results = data;
+  } else {
+    results.pages = Math.ceil(data.length / limit);
+    results.results = data.slice(start, end);
+  }
   return results;
 };
 
 const fetchApi = () => {
-  const bodySend = {
+  const body = {
     queryString: "banks",
     resultContext: {
       aspects: ["title", "lifecycle", "location", "summary", "editorial"],
@@ -42,7 +48,7 @@ const fetchApi = () => {
   };
   const options = {
     method: "POST",
-    body: JSON.stringify(bodySend),
+    body: JSON.stringify(body),
     headers: {
       "Content-Type": "application/json",
       "X-Api-Key": "59cbaf20e3e06d3565778e7b9758f7892e89468293a48663b98bd1d9",
@@ -53,9 +59,24 @@ const fetchApi = () => {
     fetch(`https://api.ft.com/content/search/v1`, options)
       .then((res) => res.json())
       .then((data) => {
+        let results = [];
         const resultData = data.results[0].results;
+        const searchKey = req.query.key;
 
-        const results = paginatedResults(req, resultData);
+        if (searchKey) {
+          const searchedResults = resultData.filter((el) => {
+            return (
+              el.title.title.toLowerCase().indexOf(searchKey.toLowerCase()) >= 0 ||
+              el.summary.excerpt.toLowerCase().indexOf(searchKey.toLowerCase()) >= 0 ||
+              el.editorial.subheading.toLowerCase().indexOf(searchKey.toLowerCase()) >= 0
+            );
+          });
+          results = paginatedResults(req, searchedResults);
+        } else {
+          results = paginatedResults(req, resultData);
+        }
+        results.page = parseInt(req.query.page);
+        results.limit = parseInt(req.query.limit);
 
         res.resultApi = results;
         next();
@@ -66,6 +87,11 @@ const fetchApi = () => {
 app.get("/api", fetchApi(), (req, res) => {
   res.json(res.resultApi);
 });
+
+app.get("/search/", fetchApi(), (req, res) => {
+  res.json(res.resultApi);
+});
+
 const listener = app.listen(port, function () {
   console.log("server is listtening to the prot: " + listener.address().port);
 });
